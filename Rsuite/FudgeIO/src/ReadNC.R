542dea403cb25b1312575cab1aee2fdadeae2e67
Carolyn Whitlock <carolyn.whitlock@noaa.gov>
2014-08-11 13:57:07 -0400

    collapse_degen option added to ReadNC, minor changes to examples

--- Rsuite/FudgeIO/src/ReadNC.R
+++ Rsuite/FudgeIO/src/ReadNC.R
@@ -2,16 +2,22 @@ ReadNC <- function(nc.object,var.name=NA,dstart=NA,dcount=NA,verbose=FALSE) {
   #'Reads data from netCDF file object
   #'Returns netCDF variable data
   #'uses get.var.ncdf
-  #'Returned values will be in ordinary R double precision if the netCDF variable type is float or double. Returned values will be in R's integer storage mode if the netCDF variable type is short or int. Returned values will be of character type if the netCDF variable is of character type.
-  #'[count]: A vector of integers indicating the count of values to read along each dimen- sion (order is X-Y-Z-T). 
-  #'[start]: A vector of indices indicating where to start reading the passed values (begin- ning at 1). The length of this vector must equal the number of dimensions the variable has. Order is X-Y-Z-T (i.e., the time dimension is last). If not specified, reading starts at the beginning of the file (1,1,1,...). 
+  #'Returned values will be in ordinary R double precision if the netCDF variable type is float or double. 
+  #'Returned values will be in R's integer storage mode if the netCDF variable type is short or int. 
+  #'Returned values will be of character type if the netCDF variable is of character type.
+  #'And finally, all returned values will have 3 dimensions, even if the lenght of those dimensions is 1
+  #'(i.e. a single data point has the dimensions c(1,1,1))
+  #'@param [count]: A vector of integers indicating the count of values to read along each dimen- sion (order is X-Y-Z-T). 
+  #'@param [start]: A vector of indices indicating where to start reading the passed values (begin- ning at 1). 
+  #'The length of this vector must equal the number of dimensions the variable has. Order is X-Y-Z-T (i.e., the time dimension is last).
+  #'If not specified, reading starts at the beginning of the file (1,1,1,...). 
   if((is.na(dstart)) && (is.na(dcount)) && (is.na(var.name))) {
   print("check 1")
   read.nc <- ncvar_get(nc.object)            
   }else {
   print("check 2")
   print(dcount)
-  var.read <- ncvar_get(nc.object,var.name,dstart,dcount) 
+  var.read <- ncvar_get(nc.object,var.name,dstart,dcount, collapse_degen=FALSE) #collapse_degen ensures that all datasets are 3-D
   }
 #  nc_close(nc.object) #Creates an error when called again if left uncommented
   return(var.read)
--- Rsuite/FudgeIO/src/examples/sample_fudge_io_script.R
+++ Rsuite/FudgeIO/src/examples/sample_fudge_io_script.R
@@ -6,14 +6,15 @@ source('~/Code/fudge2014/Rsuite/FudgeIO/src/ReadNC.R')
 source('~/Code/fudge2014/Rsuite/FudgeIO/src/CF.R')
 source('~/Code/fudge2014/Rsuite/FudgeIO/src/OpenNC.R')
 
+#grab an NC object
 nc.mine <- OpenNC("/home/cew/Code/climdex/","tasmax_sample_input.nc")
-
+#get a data point
 var.single <- ReadNC(nc.mine,"tasmax",c(1,1,1),c(1,1,1))
 print(var.single)
 #Get a data slice
 var.lonslice <- ReadNC(nc.mine,"tasmax",c(1,1,1),c(1,114,1))
 print(length(var.lonslice))
-print(dim(var.lonslice)) #Note that method returns single dim; ask Aparna about collapse_degen and dimensions
+print(dim(var.lonslice)) 
 #make alteration to data and write
 var.new<-ReadNC(nc.mine,"tasmax")
 var.new[1,]<-NA
@@ -21,9 +22,3 @@ dim(var.new) <- c(1,1,1,114, 3652)
 WriteNC("fudgeio_sample_output.nc",var.new,"tasmax" #Cf-checker removes 'tasmax_tweaked'
         ,1,seq(1:114),1,3652,    #Note indexes from 1, not 0. No dim checking. 
         1969,"","gregorian")
-##This should break
-dim(var.new) <- c(1,3652, 1,1,114)
-WriteNC("fudgeio_sample_output_break.nc",var.new,"tasmax" #Cf-checker removes 'tasmax_tweaked'
-        ,1,seq(1:114),1,3652,    #Note indexes from 1, not 0. No dim checking. 
-        1969,"","gregorian")
-###This does not break.Probably not a problem. 
