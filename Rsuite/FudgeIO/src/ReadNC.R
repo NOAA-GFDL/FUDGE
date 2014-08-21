32b8a76c2477cbec401cf5824a809e8ab19669cc
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2014-08-21 17:31:24 -0400

    ReadNC overwrite with a1r's changes

--- Rsuite/FudgeIO/src/ReadNC.R
+++ Rsuite/FudgeIO/src/ReadNC.R
@@ -3,23 +3,12 @@ ReadNC <- function(nc.object,var.name=NA,dstart=NA,dcount=NA,verbose=FALSE) {
   #'Reads data from netCDF file object
   #'Returns netCDF variable data
   #'uses get.var.ncdf
-  #'Returned values will be in ordinary R double precision if the netCDF variable type is float or double. 
-  #'Returned values will be in R's integer storage mode if the netCDF variable type is short or int. 
-  #'Returned values will be of character type if the netCDF variable is of character type.
-  #'And finally, all returned values will have 3 dimensions, even if the lenght of those dimensions is 1
-  #'(i.e. a single data point has the dimensions c(1,1,1))
-  #'@param [count]: A vector of integers indicating the count of values to read along each dimen- sion (order is X-Y-Z-T). 
-  #'@param [start]: A vector of indices indicating where to start reading the passed values (begin- ning at 1). 
-  #'The length of this vector must equal the number of dimensions the variable has. Order is X-Y-Z-T (i.e., the time dimension is last).
-  #'If not specified, reading starts at the beginning of the file (1,1,1,...). 
+  #'Returned values will be in ordinary R double precision if the netCDF variable type is float or double. Returned values will be in R's integer storage mode if the netCDF variable type is short or int. Returned values will be of character type if the netCDF variable is of character type.
+  #'[count]: A vector of integers indicating the count of values to read along each dimen- sion (order is X-Y-Z-T). 
+  #'[start]: A vector of indices indicating where to start reading the passed values (begin- ning at 1). The length of this vector must equal the number of dimensions the variable has. Order is X-Y-Z-T (i.e., the time dimension is last). If not specified, reading starts at the beginning of the file (1,1,1,...). 
   if((is.na(dstart)) && (is.na(dcount)) && (is.na(var.name))) {
   clim.in <- ncvar_get(nc.object)            
   }else {
-  var.read <- ncvar_get(nc.object,var.name,dstart,dcount,collapse_degen=FALSE)
-  }
-#  nc_close(nc.object) #Creates an error when called again if left uncommented
-  return(var.read)
-=======
   clim.in <- ncvar_get(nc.object,var.name,dstart,dcount,collapse_degen=FALSE) 
   }
   #### get standard name,long name, units if present ####
@@ -31,6 +20,8 @@ ReadNC <- function(nc.object,var.name=NA,dstart=NA,dcount=NA,verbose=FALSE) {
   units <- ncatt_get(nc.object, var.name, attname)
   #######################################################
   listout <- list("clim.in"=clim.in,"cfname"=cfname,"long_name"=long_name,"units"=units)
+  #print("debug .......")
+  #print(clim.in)
   nc_close(nc.object)
   return(listout)
 }
