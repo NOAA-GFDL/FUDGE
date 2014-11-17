1b0ac69176fdf09c901c5b2a9e6b45bfc957787a
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2014-11-17 14:52:15 -0500

    postProc twoD checker moved to perform after the results are generated. Error changed to warning.

--- utils/aux/checker.py
+++ utils/aux/checker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os,sys
-def checker(path,cnt=0):
+def checker(path,cnt=0,sev="error"):
        #actual_cnt = len(os.walk(path).next()[2])
 			
        listfile = []	 
@@ -21,9 +21,12 @@ def checker(path,cnt=0):
        if (cnt > 0):
 	 if (cnt != actual_cnt):
 	        print "Actual Count: ",actual_cnt
-		print "Expected Count: ",cnt 
-                print "\033[1;31m Error code -11: The expected number of files is not present. Please verify!\033[1;m"
-		sys.exit(-11)
+		print "Expected Count: ",cnt
+		if(sev == "error"):
+                	print "\033[1;31m Error code -11: The expected number of files is not present. Please verify!\033[1;m"
+		        sys.exit(-11)
+		else:
+	                print "\033[1;43m Warning code -11: The expected number of files is not present. Please verify!\033[1;m"
        if (len(listbad) > 0):
 	       print "\033[1;31m Error code -10 OOPS: Total number of extraneous files in"+path+"is:\033[1;m",len(listbad)
 	       print "\033[1;31mPlease verify this, clean-up directory and try again. CANNOT RUN postProc at this time.\033[1;m"
