d45c4d7fa50acd7912f7a3de754c41db0723b121
Carolyn Whitlock <carolyn.whitlock@noaa.gov>
2014-11-26 09:55:23 -0500

    Adding bad commit to test error checking in pre-push hook

--- Rsuite/Drivers/Master_Driver.R
+++ Rsuite/Drivers/Master_Driver.R
@@ -25,6 +25,9 @@ options(error=error.handler.function, warn = 1, showErrorCalls=TRUE)
 ###after this point. Probably not a component of a --vanilla run. 
 ###But it unquestionably simplifies debugging.
 
+message("Deliberately attempting to break code")
+message(fakevar)
+
 
 #------- Add libraries -------------
 LoadLib(ds.method)
