ed0c6b9f66e02e5e40aab8f653bca3e6d4ada159
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2015-02-13 12:49:22 -0500

    clean up

--- bin/expergen.py
+++ bin/expergen.py
@@ -272,18 +272,6 @@ def listVars(uinput,basedir=None,msub=False,pp=False):
                 else:
                         print "Please specify a single station. "
                         sys.exit()
-        elif((output_grid == 'US48') | (output_grid == 'us48')):
-		output_grid = "US48"
-		dim1=dim
-                dsuffix = "J454-567"
-		#CEW comment out for the PM data
-		#region = output_grid
-		dim1 = dim
-        elif(region == 'global'):
-                dsuffix = "J1-720"
-		dim1=dim
-		#A1R uncomment during PM edit; no region yet.
-		region = output_grid
         else:
 	   if(file_j_range != ''):
 	   	dsuffix=file_j_range
