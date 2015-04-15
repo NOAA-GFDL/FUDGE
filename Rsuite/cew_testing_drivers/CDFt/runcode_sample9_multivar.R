585844181cef503465f0fb12d403cb1ce2b15b95
Carolyn Whitlock <carolyn.whitlock@noaa.gov>
2015-04-15 09:59:14 -0400

    coordinating changes from oen branch runcode to another

--- Rsuite/cew_testing_drivers/CDFt/runcode_sample9_multivar.R
+++ Rsuite/cew_testing_drivers/CDFt/runcode_sample9_multivar.R
@@ -2,57 +2,58 @@
 rm(list=ls())
 
 #--------------predictor and target variable names--------#
-	predictor.vars <- 'tasmax' 
+	#predictor.vars <- list('tasmax', 'tasmin') 
+predictor.vars <- list('tasmax') 
 	target.var <- 'tasmax'
 #--------------grid region, mask settings----------#
-grid <- 'SCCSC0p1' 
-spat.mask.dir_1 <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/geomasks/red_river_0p1/OneD/' 
-spat.mask.var <- 'red_river_0p1_masks' 
-ds.region <- 'RR'
+        grid <- 'SCCSC0p1' 
+        spat.mask.dir_1 <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/geomasks/red_river_0p1/OneD/' 
+        spat.mask.var <- 'red_river_0p1_masks' 
+	ds.region <- 'RR'
 #--------------- I,J settings ----------------#
-file.j.range <- 'J31-170' 
-i.file <- 300   
-j.start <- 31 
-j.end <- 170 
-loop.start <-  j.start - (j.start-1)
-loop.end <-  j.end - (j.start-1)
+        file.j.range <- 'J31-170' 
+        i.file <- 300   
+        j.start <- 31 
+        j.end <- 170 
+        loop.start <-  j.start - (j.start-1)
+        loop.end <-  j.end - (j.start-1)
 #------------ historical predictor(s)----------# 
-hist.file.start.year_1 <- 19610101 
-hist.file.end.year_1 <- 20051231
-hist.train.start.year_1 <- 19610101
-hist.train.end.year_1 <- 20051231 
-hist.scenario_1 <- 'historical_r1i1p1'
-hist.nyrtot_1 <- (hist.train.end.year_1 - hist.train.start.year_1) + 1
-hist.model_1 <- 'MPI-ESM-LR' 
-hist.freq_1 <- 'day' 
-hist.indir_1 <- '/archive/esd/PROJECTS/DOWNSCALING//GCM_DATA/CMIP5//MPI-ESM-LR/historical/day/atmos/day/r1i1p1/v20111006/tasmax/SCCSC0p1/OneD/' 
+	hist.file.start.year_1 <- 19610101 
+	hist.file.end.year_1 <- 20051231
+        hist.train.start.year_1 <- 19610101
+	hist.train.end.year_1 <- 20051231 
+	hist.scenario_1 <- 'historical_r1i1p1'
+	hist.nyrtot_1 <- (hist.train.end.year_1 - hist.train.start.year_1) + 1
+	hist.model_1 <- 'MPI-ESM-LR' 
+	hist.freq_1 <- 'day' 
+	hist.indir_1 <- '/archive/esd/PROJECTS/DOWNSCALING//GCM_DATA/CMIP5//MPI-ESM-LR/historical/day/atmos/day/r1i1p1/v20111006' 
 #	hist.time.window <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/timemasks/maskdays_bymonth_pm2weeks_19610101-20051231_olap.nc' 
 hist.time.window <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/timemasks/maskdays_bymonth_19610101-20051231.nc'
 #------------ future predictor(s) -------------# 
-fut.file.start.year_1 <- 20060101 
-fut.file.end.year_1 <- 20991231 
-fut.train.start.year_1 <- 20060101 
-fut.train.end.year_1 <- 20991231 
-fut.scenario_1 <- 'rcp45_r1i1p1'
-fut.nyrtot_1 <- (fut.train.end.year_1 - fut.train.start.year_1) + 1
-fut.model_1 <- 'MPI-ESM-LR' 
-fut.freq_1 <- 'day' 
-fut.indir_1 <- '/archive/esd/PROJECTS/DOWNSCALING//GCM_DATA/CMIP5//MPI-ESM-LR/rcp45/day/atmos/day/r1i1p1/v20111006/tasmax/SCCSC0p1/OneD/'
-#fut.time.window <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/timemasks/maskdays_bymonth_pm2weeks_20060101-20991231_olap.nc'
+	fut.file.start.year_1 <- 20060101 
+	fut.file.end.year_1 <- 20991231 
+        fut.train.start.year_1 <- 20060101 
+        fut.train.end.year_1 <- 20991231 
+	fut.scenario_1 <- 'rcp45_r1i1p1'
+	fut.nyrtot_1 <- (fut.train.end.year_1 - fut.train.start.year_1) + 1
+	fut.model_1 <- 'MPI-ESM-LR' 
+	fut.freq_1 <- 'day' 
+	fut.indir_1 <- '/archive/esd/PROJECTS/DOWNSCALING//GCM_DATA/CMIP5//MPI-ESM-LR/rcp45/day/atmos/day/r1i1p1/v20111006'
+	#fut.time.window <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/timemasks/maskdays_bymonth_pm2weeks_20060101-20991231_olap.nc'
 fut.time.window <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/timemasks/maskdays_bymonth_20060101-20991231.nc'
-# fut.time.trim.mask <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/timemasks/maskdays_bymonth_20060101-20991231.nc'
+       # fut.time.trim.mask <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/timemasks/maskdays_bymonth_20060101-20991231.nc'
 fut.time.trim.mask <- 'na'
 #-------------- predictor directory suffix -----------#
 pred.dir.suffix <- '/SCCSC0p1/OneD/' #use this predictor variable and this  suffix with fut.indir_1 and hist.indir_1
 #------------- target -------------------------# 
-target.file.start.year_1 <- 19610101 
-target.file.end.year_1 <- 20051231 
-target.train.start.year_1 <- 19610101 
-target.train.end.year_1 <- 20051231 
-target.scenario_1 <- 'historical_r0i0p0'
-target.nyrtot_1 <- (target.train.end.year_1 - target.train.start.year_1) + 1 
-target.model_1 <- 'livneh'
-target.freq_1 <- 'day' 
+	target.file.start.year_1 <- 19610101 
+	target.file.end.year_1 <- 20051231 
+        target.train.start.year_1 <- 19610101 
+        target.train.end.year_1 <- 20051231 
+	target.scenario_1 <- 'historical_r0i0p0'
+	target.nyrtot_1 <- (target.train.end.year_1 - target.train.start.year_1) + 1 
+	target.model_1 <- 'livneh'
+	target.freq_1 <- 'day' 
 target.indir_1 <- paste0('/archive/esd/PROJECTS/DOWNSCALING//OBS_DATA/GRIDDED_OBS//livneh/historical/day/atmos/day/r0i0p0/v1p2/',
                          target.var,'/SCCSC0p1/OneD/')
 #	target.time.window <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/timemasks/maskdays_bymonth_pm2weeks_19610101-20051231_olap.nc'
@@ -60,7 +61,8 @@ target.time.window <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/timemas
 
 #------------- method name k-fold specs-----------------------#
         ds.method <- 'CDFt' 
-	ds.experiment <- 'RRtxp1-CDFt-C34atTest_monL01K00' 
+#ds.method <- 'multi.lm'
+	ds.experiment <- paste0('RRtxp1-', ds.method, '-C34atTest3L01K00')
 	k.fold <- 0 
 	
 #-------------- output -----------------------#
@@ -70,6 +72,11 @@ target.time.window <- '/archive/esd/PROJECTS/DOWNSCALING/3ToThe5th/masks/timemas
         args=list(dev=1,npas='default') 
  #Number of "cuts" for which quantiles will be empirically estimated (Default is 100 in CDFt package).
 #-------------- pp ---------------------------#
+# pr_opts=list(pr_threshold_in='us_trace',pr_freqadj_in='off',pr_conserve_in='on',
+#              pr_threshold_out='us_trace',pr_conserve_out='on', #'us_trace'
+#              apply_0_mask='off')
+
+#  pr_opts=list(pr_threshold_in='us_trace',pr_freqadj_in='off',pr_conserve_in='on',pr_threshold_out='us_trace',pr_conserve_out='on')
         #mask.list <- list(mask1=list(type='SBiasCorr',adjust.out='off',qc.mask='on',qc_options=list(botlim=-6.,toplim=6.)))
 mask.list <- list("na")
 ################### others ###################################
